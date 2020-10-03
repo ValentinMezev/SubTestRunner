ddd1bdb6602edc20140c7673ea871fe696cc3fdd
GitHub Action <action@github.com>
2020-10-03 11:53:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/valentinmezev/subtestrunner/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.valentinmezev.subtestrunner.services
+
+import com.github.valentinmezev.subtestrunner.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
